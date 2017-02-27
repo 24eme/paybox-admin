bc25dc40354465c6bb91a94327bb588f77149435
Gabriel Poma <gabriel.poma@vet-alfort.fr>
2017-02-27 11:49:05 +0100

    Ajout authentification http

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,11 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+use Tuupola\Middleware\HttpBasicAuthentication;
+
+$app->add(new HttpBasicAuthentication([
+	"users" => [
+		"administrator" => getenv("ADMIN_PASSWORD")
+	]
+]));
